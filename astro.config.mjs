e8713a90747f6864aac8e006a7ac32b59ac5209e
stampaaaron <aaron@stampa.ch>
2025-10-02 14:40:00 +0000

    chore: enhance navigation using base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react()],
+  base: "ronkies.ch/",
+  trailingSlash: "never"
 });
