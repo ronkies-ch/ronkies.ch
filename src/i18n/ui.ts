1a8717cafeb90803abf038220f659b8316bc15b1
stampaaaron <aaron@stampa.ch>
2025-11-20 15:06:06 +0000

    feat: improve language picker layout

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -1,17 +1,18 @@
 export const languages = {
-    'de-CH': 'Schwiizerdütsch',
-    'de-DE': 'Hochdeutsch',
+	"de-CH": "CH",
+	"de-DE": "DE",
 };
 
-export const locales = Object.keys(languages) as (keyof typeof languages)[]
+export const languageIcons: Record<keyof typeof languages, string> = {
+	"de-CH": "🇨🇭",
+	"de-DE": "🇩🇪",
+};
 
-export const defaultLocale: keyof typeof languages = 'de-CH';
+export const locales = Object.keys(languages) as (keyof typeof languages)[];
+
+export const defaultLocale: keyof typeof languages = "de-CH";
 
 export const ui: Record<keyof typeof languages, Record<string, string>> = {
-    "de-CH": {
-
-    },
-    "de-DE": {
-
-    },
+	"de-CH": {},
+	"de-DE": {},
 } as const;
