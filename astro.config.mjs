2b1c0762a84344d67249d8526f2092b25d0669cc
stampaaaron <aaron@stampa.ch>
2026-01-11 10:52:54 +0000

    chore: add configuration for search engine indexing

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,11 @@ import icon from "astro-icon";
 import alpinejs from "@astrojs/alpinejs";
 import { defaultLocale, locales } from './src/i18n/ui';
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon(), alpinejs()],
+  integrations: [icon(), alpinejs(), sitemap()],
   trailingSlash: "never",
   i18n: {
     locales,
